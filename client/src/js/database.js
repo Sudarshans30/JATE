490044263e16ef4feee75838a32eaf9f9d92dcaa
sudarshans471-pqqgaF <sudarshans471@gmail.com>
2023-04-08 09:56:12 +1000

    corrected code for webpack config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,25 +12,47 @@ const initdb = async () =>
     },
   });
 
-export const putDb = async (content) => {
-  const txtEditorDB = await openDB('jate', 1);
-  const transVar = txtEditorDB.transaction('jate', 'readwrite');
-  const storeVar = transVar.objectStore('jate');
-  const request = storeVar.put({ id: 1, value: content });
-
-  const result = await request;
-  console.log('Data saved to database', result.value);
-};
-
-
-export const getDb = async () => {
-  const txtEditorDB = await openDB('jate', 'readonly');
-  const storeVar = trasnVar.objectStore('jate');
-  const request = storeVar.get(1);
-  const result = await request;
-  result
-    ? console.log('Data retrieved from the database', result.value)
-    : console.log('Data not found in the database');
-};
+  export const putDb = async (content)  => {
+    console.log('Post to the database');
+  
+    // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB('jate', 1);
+  
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readwrite');
+  
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+  
+    // Use the .add() method on the store and pass in the content.
+    const request = store.add({ id: 1, value: content });
+  
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  };
+  ;
+  
+  // Export a function we will use to GET to the database.
+  export const getDb = async () => {
+    console.log('GET from the database');
+  
+    // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB('jate', 1);
+  
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readonly');
+  
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+  
+    // Use the .getAll() method to get all data in the database.
+    const request = store.get(1);
+  
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+  };
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,24 +1,32 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.defferedPrompt = event;
+
+    // Store the triggered events
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
-});
+  });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const promptEvent =window.defferedPrompt;
-    if (!promptEvent) {
-        return;
-    }
-    promptEvent.prompt();
-    window.defferedPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+  
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.defferedPrompt = null;
-});
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
